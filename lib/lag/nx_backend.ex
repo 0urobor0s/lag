498b817aae58a574044c561c7c53113923f510aa
0urobor0s <0urobor0s@users.noreply.github.com>
2023-11-12 16:12:15 +0000

    Min-plus matrix multiplication

--- lib/lag/nx_backend.ex
+++ lib/lag/nx_backend.ex
@@ -19,6 +19,42 @@ defmodule LAG.NxBackend do
       |> Nx.as_type(vertices.type)
   end
 
+  defnp modified_incidence_matrix_n(adjacency_matrix) do
+    # Should check if the value is `Nx.Constants.max_finite(Nx.type(adjacency_matrix))`
+    # and warn accordingly
+    max = Nx.sum(adjacency_matrix) + 1
+    {n, _} = Nx.shape(adjacency_matrix)
+
+    Nx.select(adjacency_matrix, adjacency_matrix, max)
+    # Does not support self loops
+    |> Nx.put_diagonal(Nx.broadcast(0, {n}))
+  end
+
+  # Min-plus matrix multiplication, also known as min-plus products or distance product.
+  deftransform min_plus_dot(%Graph{} = graph) do
+    graph.adjacency_matrix
+    |> modified_incidence_matrix_n()
+    |> min_plus_dot_n()
+  end
+
+  defn min_plus_dot_n(a, b) do
+    {n, _} = Nx.shape(a)
+
+    while {a, b, hop = Nx.broadcast(0, {n, n}), d = Nx.broadcast(0, {n, n})}, i <- 0..(n - 1) do
+      ri = Nx.slice(a, [i, 0], [1, n]) |> Nx.transpose()
+      add_ci = Nx.add(ri, b)
+      min_ci = add_ci |> Nx.reduce_min(axes: [0], keep_axes: true)
+      min_ki = add_ci |> Nx.argmin(axis: 0, keep_axis: true)
+
+      {a, b, Nx.put_slice(hop, [i, 0], min_ci), Nx.put_slice(d, [i, 0], min_ki)}
+    end
+    |> then(&{elem(&1, 2), elem(&1, 3)})
+  end
+
+  defn min_plus_dot_n(a) do
+    min_plus_dot_n(a, a)
+  end
+
   # Currently no self node edge is support as it should count twice
   @impl true
   deftransform degree_matrix(%Graph{} = graph) do
