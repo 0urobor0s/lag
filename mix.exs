60ce5655c74be66e95dd6d7a29e816ac0f85f17c
0urobor0s <0urobor0s@users.noreply.github.com>
2023-11-12 14:25:48 +0000

    Prepare release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule LAG.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/0urobor0s/lag"
+  @version "0.0.1"
+
   def project do
     [
       app: :lag,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "LAG (Linear Algebra Graph)",
+      package: package(),
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      name: "LAG",
+      source_url: @source_url
     ]
   end
 
@@ -26,6 +34,17 @@ defmodule LAG.MixProject do
       # {:nx, github: "elixir-nx/nx", sparse: "nx", override: true, branch: "v0.6"},
       {:exla, "~> 0.6", optional: true},
       {:benchee, "~> 1.0", only: :dev}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ~w(lib test/support)
+  defp elixirc_paths(_), do: ~w(lib)
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
